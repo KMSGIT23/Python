b75f358f864a17305d3ffb555835858c03d2ec4e
김민성 <128460782+KMSGIT23@users.noreply.github.com>
2023-10-07 23:59:11 +0900

    Add files via upload

---
+++ 56.py
@@ -0,0 +1,14 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = 'https://wikidocs.net/book/2'
+
+with urllib.request.urlopen(url) as f:
+    html = f.read().decode('utf-8')
+
+soup = BeautifulSoup(html, 'html.parser')
+titles = soup.select('.list-group-item > span')
+
+for title in titles[1:]:
+    s = title.select('span')[0].text.strip()
+    print(s)
